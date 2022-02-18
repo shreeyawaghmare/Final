5f422a688318cda367676be68acd13b641657212
Shreeya Waghmare <shreeyawaghmare4@gmail.com>
2022-02-18 19:17:06 +0530

    commit

---
+++ UsingFinalKeyword/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//creating a final variable
+		final int AGE =24;
+		//trying to change the final variable
+		 AGE = 26;
+		System.out.println("Age: " + AGE);
+		
+	}
+
+}
